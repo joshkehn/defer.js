a78a590fe2412c8688df9587ebac094fe23a968e
Joshua Kehn <josh.kehn@gmail.com>
2011-09-20 13:42:51 -0400

    Added defer.js and README

---
+++ README.md
@@ -0,0 +1,39 @@
+# DeferJS
+
+Provides an easy deferrment pattern for evented systems.
+
+## TODO's
+
+1. Testing
+2. More testing
+3. Allow arguments from the event
+
+## Usage
+
+The include pattern differs slightly from the client to the browser.
+
+    // Node.js
+    var Defer = require('deferjs').Defer;
+
+    // Browser
+    <script type="text/javascript" src="defer.js"></script>
+
+Actual usage is fairly straight-forward.
+
+    var def = new Defer();
+
+    process.on('event', def.trigger);
+
+    def.defer(function () {
+    console.log('Event happened.');
+    });
+
+    def.defer(function () {
+        setTimeout(function () {
+            console.log('This does not block.');
+        }, 1500);
+    });
+
+### About
+
+Written by [Joshua Kehn](http://joshuakehn.com/).
---
+++ defer.js
@@ -0,0 +1,41 @@
+;(function (root) {
+
+    // Setup `next` to use `process.nextTick` if it's there and `setTimeout` if not.
+    var next = (typeof process === 'undefined' || !process.nextTick) ? function (fn) { setTimeout(fn, 0); } : process.nextTick;
+
+    // Main export
+    root.Defer = function () {
+        // Keep track of our context and any deferred functions.
+        var udef, context, deferred = [];
+
+        // ### Defer.trigger
+        // Triggers any functions on the deferred stack.
+        this.trigger = function trigger () {
+            context = this;
+            deferred.forEach(function (fn) {
+                next(fn.bind(context));
+            });
+            deferred = [];
+        };
+
+        // ### Defer.defer (fn)
+        // #### fn `{Function}` Function to defer execution of
+        // If the event has been called execute `fn` immeditely. Otherwise wait until `trigger` is called.
+        this.defer = function defer (fn) {
+            if (context) {
+                next(fn.bind(context));
+            } else {
+                deferred.push(fn);
+            }
+        };
+
+        // ### Defer.reset
+        // Resets the `trigger`. Useful for staging multiple deferred functions and then restacking more for
+        // another event.
+        this.reset = function reset () {
+            context = udef;
+        };
+
+        return this;
+    };
+}(typeof exports === "undefined" ? window : exports));
